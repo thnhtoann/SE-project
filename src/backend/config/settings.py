7cc6cdd108808a57088872483e9a45dad87ea315
Nhut <ldnhut2420@clc.fitus.edu.vn>
2026-08-05 23:40:05 +0700

    feat(pos): initialize POS module

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'core',
     'omnichannel',
+    'pos',
 ]
 
 MIDDLEWARE = [
--- src/backend/config/urls.py
+++ src/backend/config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('api/webhooks/', include('omnichannel.urls')),
+    path("api/pos/", include("pos.urls")),
 ]
